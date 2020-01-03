fb152ad76c586d4d5bc7532900ce52418472ed3b
qiisziilbash <m.shafiei0423@gmail.com>
2020-01-03 13:53:46 -0700

    error in settings file is debugged

--- accounting/accounting/settings.py
+++ accounting/accounting/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounting.accounts'
+    'accounts'
 ]
 
 MIDDLEWARE = [
