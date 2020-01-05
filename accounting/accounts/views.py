4ed4acbaf0782cfb8f1aaf387345453cc2c1bff9
qiisziilbash <m.shafiei0423@gmail.com>
2020-01-05 16:03:13 -0700

    login and register pages were created

--- accounting/accounts/views.py
+++ accounting/accounts/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 
 def sign_in(request):
+    if request.method is 'GET':
+        return render(request, 'accounts/Login.html')
     return None
 
 
 def sign_up(request):
+    if request.method is 'POST':
+        return render(request, 'accounts/Register.html')
     return None
