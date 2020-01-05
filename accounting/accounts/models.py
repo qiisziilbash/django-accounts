a451a26d74d61728bb5c90789c913cba43265331
qiisziilbash <m.shafiei0423@gmail.com>
2020-01-05 15:43:59 -0700

    user profile model is created

--- accounting/accounts/models.py
+++ accounting/accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(null=True)
+
+    verification_code = models.IntegerField(default=0)
+    code_expiry_date = models.DateTimeField(null=True)
+    is_email_verified = models.BooleanField(default=False)
+
+    security_question = models.CharField(max_length=100, null=True)
+    security_answer = models.CharField(max_length=100, null=True)
